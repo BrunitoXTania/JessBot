6a29ed9e557c489a77fd18e3d88b929cb636c89e
BrunitoXTania <145606165+BrunitoXTania@users.noreply.github.com>
2023-11-11 10:15:37 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,12 +5,12 @@ throw false
 }
 let pesan = args.join` `
 let oi = `ღ ${lenguajeGB['smsAddB5']()} ${pesan}`
-let teks = `╭━〔 *${lenguajeGB['smstagaa']()}* 〕━⬣\n\n${oi}\n\n`
+let teks = `𝑨𝑹𝑹𝑰𝑩𝑨 𝑷𝑳𝑨𝑵𝑻𝑨𝑺🌱\n\n❏ @𝑻𝒆𝒂𝒎.𝑮𝒊𝒓𝒍𝒔_𝑭𝒊𝒙𝒂𝒔𝒔:\n❏ 𝑬𝒕𝒊𝒒𝒖𝒆𝒕𝒂 𝒅𝒆 𝒑𝒖𝒕𝒂𝒔:\n`
 for (let mem of participants) {
-teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
+teks += `┃💛❗ @${mem.id.split('@')[0]}\n`}
 teks += `┃\n`
 teks += `┃ ${wm}\n`
-teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
+teks += `╰━━━━━[ 𝑱𝒆𝒔𝒔𝑿𝑻𝒊𝒏𝒂 ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
